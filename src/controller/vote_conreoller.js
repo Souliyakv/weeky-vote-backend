41c74bdf9a3a9e7c0b2e8fba2e29ae9e8e48b234
Souliyakv <87403110+Souliyakv@users.noreply.github.com>
2022-12-11 13:16:19 +0700

    ready for checking

--- src/controller/vote_conreoller.js
+++ src/controller/vote_conreoller.js
@@ -4,11 +4,13 @@ import { CHECKMEMBEROFTEAM } from "../model/memberofteam.js";
 import {
   CHECKVOTE,
   GETALLCOMMENTOFUSER,
+  GETMYCOMMENT,
+  GETMYSUM,
   GETSUMPOINT,
   VOTE,
 } from "../model/vote.js";
 
-//  give point and feetback to user
+//  give the point and feetback to users. 
 
 export const Vote_Controller = async (req, res) => {
   try {
@@ -16,6 +18,7 @@ export const Vote_Controller = async (req, res) => {
     if (!team_id) return res.json({ msg: "ກະລຸນາເລືອກທີມຂອງເຈົ້າ" });
     if (!user_id) return res.json({ msg: "ກະລຸນາເລືອກຄົນທີ່ເຈົ້າຕ້ອງການໂຫວດ" });
     if (!point) return res.json({ msg: "ກະລຸນາເລືອກຄະແນນທີ່ຕ້ອງການໂຫວດ" });
+    // Check input point must is not more than 10. 
     if (parseInt(point) > 10)
       return res.json({ msg: "ຄະແນນຕ້ອງຕ່ຳກວ່າ 10 ຄະແນນ" });
 
@@ -25,7 +28,7 @@ export const Vote_Controller = async (req, res) => {
     const con = getConnection();
     const month = parseInt(new Date().getMonth() + 1);
 
-    //  set date to database for check point and feetback
+    //  Setting date in database to check point and feetback
 
     const voteDay =
       new Date().getFullYear() + "/" + month + "/" + new Date().getDate();
@@ -35,10 +38,12 @@ export const Vote_Controller = async (req, res) => {
       if (result === undefined || result.length <= 0) {
         return res.json({ msg: "ບໍ່ສາມາດໃຫ້ຄະແນນໄດ້" });
       } else {
-      
+
+        // check day in vote day 
         const comment_day = result[0].comment_day;
-       
-        if(new Date().getDay() != comment_day) return res.json({msg:"ບໍ່ສາມາດໃຫ້ຄະແນນໄດ້ໃນມື້ນີ້"});
+
+        if (new Date().getDay() != comment_day)
+          return res.json({ msg: "ບໍ່ສາມາດໃຫ້ຄະແນນໄດ້ໃນມື້ນີ້" });
         con.query(
           CHECKVOTE,
           [team_id, USER_ID, user_id, voteDay],
@@ -65,31 +70,47 @@ export const Vote_Controller = async (req, res) => {
           }
         );
       }
-    });
+    },
+    );
   } catch (error) {
     return console.log(error);
   }
 };
 
-//  see all point and feetback of user
+// See your own point
 
-export const GetAllSum_Controller = async (req, res) => {
+export const GetMySum_Controller = async (req, res) => {
   try {
     const USER_ID = await authID(req.headers["token"]);
-    const team_id = req.body.team_id;
-    if (!team_id) return res.json({ msg: "ກະລຸນາເລືອກຫ້ອງຂອງເຈົ້າ" });
     const con = getConnection();
-    con.query(GETSUMPOINT, [team_id, USER_ID], (err, result) => {
+    con.query(GETMYCOMMENT, [USER_ID], (err, result) => {
       if (err) throw err;
-      return res.json({
-        type: "success",
-        result: result,
+      const count = result.length;
+      con.query(GETMYSUM, [USER_ID], (err, result) => {
+        if (err) throw err;
+        if (result === undefined || result.length <= 0) {
+          return res.json({ msg: "ບໍ່ມີຂໍ້ມູນ" });
+        } else {
+          const total = result[0].sumpoint / count;
+          let percent = total * 10;
+          if (result[0].sumpoint == null) {
+            result[0].sumpoint = 0;
+            percent = 0;
+          }
+          return res.json({
+            type: "success",
+            percent: percent,
+            sumpoint: result[0].sumpoint,
+            email: result[0].email,
+          });
+        }
       });
     });
   } catch (error) {
     return console.log(error);
   }
 };
+//  See all point and feetback of someuser.
 
 export const GetAllCommentOfUser_Controller = async (req, res) => {
   try {
@@ -108,14 +129,18 @@ export const GetAllCommentOfUser_Controller = async (req, res) => {
         con.query(GETALLCOMMENTOFUSER, [team_id, user_id], (err, result) => {
           if (err) throw err;
           const allcomment = result;
+          const count = result.length;
           con.query(GETSUMPOINT, [team_id, user_id], (err, result) => {
             if (err) throw err;
             if (result === undefined || result.length <= 0) {
               return res.json({ msg: "ບໍ່ມີຂໍ້ມູນ" });
             } else {
+              const percent = (result[0].sumpoint / count) * 10;
+
               return res.json({
                 type: "success",
                 sumpoint: result[0].sumpoint,
+                percent: percent,
                 result: allcomment,
               });
             }
